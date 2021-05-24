69f401efa7a153844631dfa8baf3b9697eb1943a
Kazion500 <patrickckabwe@gmail.com>
2021-05-24 22:31:29 +0200

    jsconfig fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: theme => ({
-        'hero-pattern': "url('http://charity-is-hope-html.themerex.net/images/slider_1_2.jpg')",
-      })
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
